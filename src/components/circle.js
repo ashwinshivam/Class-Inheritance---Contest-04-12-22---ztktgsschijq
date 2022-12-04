589fabab5ab1afecb502ca17f9ecb35904017e5e
ashwinshivam <111960991+ashwinshivam@users.noreply.github.com>
2022-12-04 14:35:24 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,15 @@
 // import using require
 
 // declare class
-
+var Shape= require('./shape);
+ class Circle extends Shape{
+                   constructor(name){
+  super(name);
+}
+calculateArea(radius){
+  return Math.PI*Math.pow(radius,2);
+}
+}
+module.exports=Circle;
 
 // export class using module.exports
