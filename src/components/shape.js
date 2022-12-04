fc0ef449e1d5ae932d6ae45d7c73ef6b68a0fe5c
ashwinshivam <111960991+ashwinshivam@users.noreply.github.com>
2022-12-04 14:44:50 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,18 @@
 // declare class
 
 // export class using module.exports
+class Shape{
+  constructor(name){
+    this.name;
+  }
+  drawShape(){
+    return `${this.name}is drawn`;
+    }
+    calculateArea(length,width){
+    return 2*(length*width);
+    }
+    colour(colour){
+    return `${colour} is Applied on ${this.name}`;
+    }
+    }
+    module.exports = Shape;
